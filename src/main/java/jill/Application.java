f9a9ac32d801beff4a0d28636582064e7a51fcbe
Administrator <897615138@qq.com>
2020-09-01 12:19:13 +0800

    1

--- src/main/java/jill/Application.java
+++ src/main/java/jill/Application.java
@@ -3,6 +3,10 @@ package jill;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * 启动类
@@ -10,6 +14,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("jill.dao")
 public class Application {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8082","http://localhost:8081","http://localhost:8080","http://localhost:8888")  //可访问ip，ip最好从配置文件中获取，
+                        .allowedMethods("PUT", "DELETE","GET","POST")
+                        .allowedHeaders("*")
+                        .exposedHeaders("access-control-allow-headers",
+                                "access-control-allow-methods",
+                                "access-control-allow-origin",
+                                "access-control-max-age",
+                                "X-Frame-Options")
+                        .allowCredentials(false).maxAge(3600);
+            }
+        };
+
+    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
     }
